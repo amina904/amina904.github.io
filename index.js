f98fca3e6e7490f4ba200494f72764ab4e8f7fb2
amina904 <amiinaaaa24@gmail.com>
2023-12-07 14:41:56 -0500

    fin de mon projet

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-//const
+/*
+cette partie cest pour les attributs et les const(variables qui peuvent pas changer)
+*/
 const container = document.getElementById("alphabetButtons");
 var answerDisplay = document.getElementById("hold");
 var answer = "";
 var hint = "";
-var life = 10;
+var life = 25;
 var wordDisplay = [];
 var winningCheck = "";
 const containerHint = document.getElementById("clue");
@@ -13,6 +15,9 @@ const livesDisplay = document.getElementById("mylives");
 var myStickman = document.getElementById("stickman");
 var context = myStickman.getContext("2d");
 
+/*
+cette permets de générer les boutons du clavier
+*/
 //generate alphabet button
 function generateButton() {
   var buttonsHTML = "abcdefghijklmnopqrstuvwxyz"
@@ -42,54 +47,59 @@ function handleClick(event) {
   return;
 }
 
-//word array
+/**
+ * * j'ai ajouté mes propres mots et mes indices aux tableaux aux variables const qui ne peuvent pas changer 
+  */ 
 const question = [
-  "The Chosen Category Is Premier League Football Teams",
-  "The Chosen Category Is Films",
-  "The Chosen Category Is Cities"
+  "La catégorie choisie est celle des animaux",
+  "La catégorie choisie est celle des films",
+  "La catégorie choisie est celle des fruits et légumes"
 ];
 
 const categories = [
   [
-    "everton",
-    "liverpool",
-    "swansea",
-    "chelsea",
-    "hull",
-    "manchester-city",
-    "newcastle-united"
+    "lion",
+    "serpent",
+    "chien",
+    "panda",
+    "ours",
+    "elephant",
+    "zebre"
   ],
-  ["alien", "dirty-harry", "gladiator", "finding-nemo", "jaws"],
-  ["manchester", "milan", "madrid", "amsterdam", "prague"]
+  ["candy-man", "harry-potter", "black-panthere", "spider-man", "Ca", "la-petite-sirene"],
+
+  ["Banane", "fraise", "carotte", "pomme", "citron"]
 ];
 
 const hints = [
   [
-    "Based in Mersyside",
-    "Based in Mersyside",
-    "First Welsh team to reach the Premier Leauge",
-    "Owned by A russian Billionaire",
-    "Once managed by Phil Brown",
-    "2013 FA Cup runners up",
-    "Gazza's first club"
+    "rugis",
+    "psss",
+    "Meilleur ami",
+    "doux",
+    "aime les poissons",
+    "gros",
+    "raillure"
   ],
   [
-    "Science-Fiction horror film",
-    "1971 American action film",
-    "Historical drama",
-    "Anamated Fish",
-    "Giant great white shark"
+    "Film d’horreur de science-fiction",
+    "film pour les sorciers",
+    "tigre noir" ,
+    "Toile d'araignee",
+    "Clown",
+    "jeunesse"
   ],
   [
-    "Northern city in the UK",
-    "Home of AC and Inter",
-    "Spanish capital",
-    "Netherlands capital",
-    "Czech Republic capital"
+    "singe",
+    "charlotte-?",
+    "orange et grand",
+    "blanche neige",
+    "jaune acide"
   ]
 ];
-
-//set question,answer and hint
+/*
+cette partie permets de définir la question, la réponse et l’indice
+*/
 
 function setAnswer() {
   const categoryOrder = Math.floor(Math.random() * categories.length);
@@ -106,7 +116,9 @@ function setAnswer() {
   hint = hints[categoryOrder][wordOrder];
   answerDisplay.innerHTML = generateAnswerDisplay(chosenWord);
 }
-
+/*
+cette partie construit un nouvel affichage du mot en remplancant chaque lettre par un "-"
+*/
 function generateAnswerDisplay(word) {
   var wordArray = word.split("");
   //console.log(wordArray);
@@ -120,22 +132,29 @@ function generateAnswerDisplay(word) {
   return wordDisplay.join(" ");
 }
 
+/*
+cette partie affiche un indice 
+*/
 function showHint() {
-  containerHint.innerHTML = `Clue - ${hint}`;
+  containerHint.innerHTML = `indice - ${hint}`;
 }
 
 buttonHint.addEventListener("click", showHint);
-//setting initial condition
+
+/**
+ * Définition de la condition initiale qui veut dire que les éléments dans ce code sont configurés pour commencer une nouvelle partie
+ */
 function init() {
   answer = "";
   hint = "";
-  life = 10;
+ life = 25;
+ 
   wordDisplay = [];
   winningCheck = "";
   context.clearRect(0, 0, 400, 400);
   canvas();
-  containerHint.innerHTML = `Clue -`;
-  livesDisplay.innerHTML = `You have ${life} lives!`;
+  containerHint.innerHTML = `Indice -`;
+  livesDisplay.innerHTML = `Tu as ${life} vies!`;
   setAnswer();
   container.innerHTML = generateButton();
   container.addEventListener("click", handleClick);
@@ -145,16 +164,21 @@ function init() {
 
 window.onload = init();
 
-//reset (play again)
+/**
+ * Réinitialiser (rejouer)
+ */
+
 buttonReset.addEventListener("click", init);
 
-//guess click
+/**
+ * cette partie met a jour l'affichage du mot en cours verifie les vies et dit si le joueur a gagné ou perdu
+ */
 function guess(event) {
   const guessWord = event.target.id;
   const answerArray = answer.split("");
   var counter = 0;
   if (answer === winningCheck) {
-    livesDisplay.innerHTML = `YOU WIN!`;
+    livesDisplay.innerHTML = `TU AS GAGNÉ!`;
     return;
   } else {
     if (life > 0) {
@@ -176,11 +200,11 @@ function guess(event) {
         counter = 0;
       }
       if (life > 1) {
-        livesDisplay.innerHTML = `You have ${life} lives!`;
+        livesDisplay.innerHTML = `Tu as ${life} vies!`;
       } else if (life === 1) {
-        livesDisplay.innerHTML = `You have ${life} life!`;
+        livesDisplay.innerHTML = `Tu as ${life} vie!`;
       } else {
-        livesDisplay.innerHTML = `GAME OVER!`;
+        livesDisplay.innerHTML = `FIN DU JEU!`;
       }
     } else {
       return;
@@ -189,7 +213,7 @@ function guess(event) {
     //console.log(counter);
     //console.log(life);
     if (answer === winningCheck) {
-      livesDisplay.innerHTML = `YOU WIN!`;
+      livesDisplay.innerHTML = `TU AS GAGNÉ!`;
       return;
     }
   }
@@ -197,12 +221,21 @@ function guess(event) {
 
 container.addEventListener("click", guess);
 
-// Hangman
+/* 
+cette partie cest pour l'affichage du Bonhomme
+ */ 
 function animate() {
+  console.log(life);
   drawArray[life]();
   //console.log(drawArray[life]);
 }
+/*
+cette partie permet de dessiner des formes et appliquer les styles
 
+*/
+/*
+cette partie est appelé pour les dessins
+*/
 function canvas() {
   myStickman = document.getElementById("stickman");
   context = myStickman.getContext("2d");
@@ -211,6 +244,10 @@ function canvas() {
   context.lineWidth = 2;
 }
 
+/*
+cette partie permet dafficher la tete du bonhomme
+
+*/
 function head() {
   myStickman = document.getElementById("stickman");
   context = myStickman.getContext("2d");
@@ -223,6 +260,29 @@ function draw($pathFromx, $pathFromy, $pathTox, $pathToy) {
   context.moveTo($pathFromx, $pathFromy);
   context.lineTo($pathTox, $pathToy);
   context.stroke();
+}
+
+function head2() {
+  myStickman = document.getElementById("stickman");
+  context = myStickman.getContext("2d");
+  context.beginPath();
+  context.arc(60, 25, 10, 0, Math.PI * 2, true);
+  context.stroke();
+}
+function frame10(){
+  draw(0, 300, 300, 300);
+}
+function frame11(){
+  draw(50, 10, 20, 20);
+}
+function frame12(){
+  draw(60, 5, 60, 15);
+}
+function frame13(){
+  draw(50, 10, 20, 20);
+}
+function frame14(){
+  draw(50, 10, 20, 20);
 }
 
 function frame1() {
@@ -260,8 +320,16 @@ function rightLeg() {
 function leftLeg() {
   draw(60, 70, 20, 100);
 }
-
+/**
+ * sur cette partie jai ajouté des fonctions pour que le jeu ai plus de dessin du pendu
+ */
 var drawArray = [
+  frame10,
+  frame11,
+  frame12,
+  frame13,
+  frame14,
+  head2,
   rightLeg,
   leftLeg,
   rightArm,
@@ -272,4 +340,85 @@ var drawArray = [
   frame3,
   frame2,
   frame1
+  
+  
 ];
+
+/**
+ * quand le joueur appuie sur le bouton facile il a droit a 20 vies et s'il les epuises avant la fin du jeu il perd
+ */
+function appuieBouttonD(){
+  //alert('le bouton est cliqué');
+  life = 20;
+  wordDisplay = [];
+  winningCheck = "";
+   context.clearRect(0, 0, 400, 400);
+   canvas();
+   containerHint.innerHTML = `Indice -`;
+  livesDisplay.innerHTML = `Tu as ${life} vies!`;
+  setAnswer();
+   container.innerHTML = generateButton();
+  container.addEventListener("click", handleClick);
+  console.log(answer);
+  containerHint.innerHTML = `Indice -`;
+  livesDisplay.innerHTML = `Tu as ${life} vies!`;
+}
+let bouton1 = document.getElementsByClassName('bouton1');
+
+bouton.addEventListener('click', appuieBouttonD );
+
+/**
+ * quand le joueur appuie sur le bouton moyen il a droit a 20 vies et s'il les epuises avant la fin du jeu il perd
+ */
+function appuieBouttonI(){
+  //alert("ca marche");
+  answer = "";
+  hint = "";
+ life = 15;
+ 
+  wordDisplay = [];
+  winningCheck = "";
+  context.clearRect(0, 0, 400, 400);
+  canvas();
+  containerHint.innerHTML = `Indice -`;
+  livesDisplay.innerHTML = `Tu as ${life} vies!`;
+  setAnswer();
+  container.innerHTML = generateButton();
+  container.addEventListener("click", handleClick);
+  console.log(answer);
+  
+  
+  
+  
+}
+let bouton2 = document.getElementsByClassName('bouton2');
+bouton.addEventListener('click', appuieBouttonI );
+
+/**
+ * quand le joueur appuie sur le bouton difficile il a droit a 10 vies et s'il les epuises avant la fin du jeu il perd
+ */
+function appuieBouttonA(){
+  /* alert("cooool"); */
+  answer = "";
+  hint = "";
+ life = 10;
+ 
+  wordDisplay = [];
+  winningCheck = "";
+  context.clearRect(0, 0, 400, 400);
+  canvas();
+  containerHint.innerHTML = `Indice -`;
+  livesDisplay.innerHTML = `Tu as ${life} vies!`;
+  setAnswer();
+  container.innerHTML = generateButton();
+  container.addEventListener("click", handleClick);
+  console.log(answer);
+  
+  
+  }
+
+
+
+let bouton3 = document.getElementsByClassName('bouton3');
+bouton.addEventListener('click', appuieBouttonA );
+
